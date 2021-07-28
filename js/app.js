0dc052700817e911033c967b8a8f6f76ecdbbabf
Misti Dinzy <mistidinzy@gmail.com>
2021-07-28 17:42:07 -0500

    add css styling

--- js/app.js
+++ js/app.js
@@ -9,10 +9,8 @@ console.log('Person said ' + game + ' to the game question!');
 
 
 
-.toLowercase()
+// .toLowercase()
 
-
- 
 // 5 yes or no questions, with lowercase validation
 // alert user with response letting them know if they got it right
 // console.log messages to notify if they are correct, then comment out and add
@@ -21,28 +19,28 @@ console.log('Person said ' + game + ' to the game question!');
 // add a personalized welcome message
 // ask them their name with a prompt
 // display their name back to them with a custom greeting
-// display their name back to them in final message to the user 
+// display their name back to them in final message to the user
 
-// js file must contain 'use strict' 
+// js file must contain 'use strict'
 // ACP
-// experiment with css 
+// experiment with css
 
 // part two
-// modify readme file as you go 
+// modify readme file as you go
 // create a top ten at bottom of html page as an ordered list
 // add work and education history as an unordered list
 
-// add 6th question - guess a number 
+// add 6th question - guess a number
 // alert if too high or too low
 // give four chances to get it right
-// after four chances, tell user correct answer, use a loop 
+// after four chances, tell user correct answer, use a loop
 
 // add a 7th question - multiple choice
 // 6 chances to get it right
 // will end when they get it right or when they run out of attempts
 // display all possible answers to user
 
-// keep track of total correct answers 
+// keep track of total correct answers
 // tell user how many they got correct out of 7
 
 // console.log questions being asked
