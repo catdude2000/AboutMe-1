6bbb612784276c2acc231ff4a9110142d7b85e23
Misti Dinzy <mistidinzy@gmail.com>
2021-07-27 20:30:11 -0500

    add js prompts

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 'use strict';
 console.log('js/app.js file is connected'); 
 
+function greeting() {
+    var name=prompt("What is your Name?");
+} 
+
+function question1() {
+    var name=prompt("");
+}
+
+function question2() {
+    var name=prompt("");
+}
+
+function question3() {
+    var name=prompt("");
+}
+
+function question4() {
+    var name=prompt("");
+}
+
+function question5() {
+    var name=prompt("");
+}
