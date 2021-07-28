633754bd7844b0249a54ce7037fd1ccb0b317843
Misti Dinzy <mistidinzy@gmail.com>
2021-07-27 20:32:09 -0500

    add function calls

--- js/app.js
+++ js/app.js
@@ -5,22 +5,34 @@ function greeting() {
     var name=prompt("What is your Name?");
 } 
 
+greeting()
+
 function question1() {
     var name=prompt("");
 }
+
+question1()
 
 function question2() {
     var name=prompt("");
 }
 
+question2()
+
 function question3() {
     var name=prompt("");
 }
+
+question3()
 
 function question4() {
     var name=prompt("");
 }
 
+question4()
+
 function question5() {
     var name=prompt("");
 }
+
+question5()
